3a7122bb6bab10e6409ee749a504f8113f825922
Yibo Yan <yiboyan@usc.edu>
2025-06-30 00:46:36 +0000

    add: util app for instructing one-time log

--- workloads/hrperf_api.h
+++ workloads/hrperf_api.h
@@ -10,9 +10,10 @@
 #define u64 uint64_t
 
 #define HRP_PMC_IOC_MAGIC 'k'
-#define HRP_PMC_IOC_START       _IO(HRP_PMC_IOC_MAGIC, 1)
-#define HRP_IOC_STOP            _IO(HRP_PMC_IOC_MAGIC, 2)
-#define HRP_PMC_IOC_TSC_FREQ    _IOR(HRP_PMC_IOC_MAGIC, 10, u64)
+#define HRP_PMC_IOC_START           _IO(HRP_PMC_IOC_MAGIC, 1)
+#define HRP_IOC_STOP                _IO(HRP_PMC_IOC_MAGIC, 2)
+#define HRP_PMC_IOC_INSTRUCTED_LOG  _IO(HRP_PMC_IOC_MAGIC, 3)
+#define HRP_PMC_IOC_TSC_FREQ        _IOR(HRP_PMC_IOC_MAGIC, 10, u64)
 
 int hrperf_start() {
     int fd;
@@ -81,6 +82,26 @@ u64 hrperf_get_tsc_freq() {
 
     close(fd);
     return tsc_freq;
+}
+
+/*
+ * Instruct the hiresperf to perform exactly one poll and log operation.
+*/
+void hrperf_instruct_log() {
+    int fd;
+
+    // Open the device file
+    fd = open("/dev/hrperf_device", O_RDWR);
+    if (fd < 0) {
+        perror("open");
+    }
+
+    // Get the TSC frequency
+    if (ioctl(fd, HRP_PMC_IOC_INSTRUCTED_LOG) < 0) {
+        perror("ioctl");
+        close(fd);
+    }
+    close(fd);
 }
 
 // for the bpf component
